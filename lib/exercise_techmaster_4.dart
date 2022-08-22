aa93d1232c19fe8d0fce4d2a36f9db3f86548642
DuongVuCoder <vuduongdc226@gmail.com>
2022-08-22 23:11:02 +0700

    first commit

--- lib/exercise_techmaster_4.dart
+++ lib/exercise_techmaster_4.dart
@@ -1,3 +1,12 @@
-int calculate() {
-  return 6 * 7;
+import 'package:exercise_techmaster_4/exercise_techmaster_4.dart'
+    as exercise_techmaster_4;
+
+void main(List<String> arguments) {
+  String str = "Happiness";
+  Map<String, int> map = {};
+  for (int i = 0; i < str.length; i++) {
+    int count = map[str[i]] ?? 0;
+    map[str[i]] = count + 1;
+  }
+  print(map);
 }
